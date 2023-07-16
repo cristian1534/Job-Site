076f5dd3b2b8967abc740282589fc84ff96eb65f
cristian1534 <cmachuca32@gmail.com>
2023-07-16 22:28:18 +0000

    Uploading Project.

--- client/src/pages/canditate/[id].js
+++ client/src/pages/canditate/[id].js
@@ -1,16 +1,13 @@
 import { useRouter } from "next/router";
-import ProfileDetails from "../../components/ProfileDetails";
-// Importa los datos del perfil o accede a ellos desde una fuente de datos
+import ProfileDetails from "../profile";
 
 const ProfilePage = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  // Aquí puedes obtener los detalles del perfil en función del ID y pasarlos a ProfileDetails
+  
 
   const profile = {
-    // Los detalles del perfil seleccionado
-    // Puedes obtener estos datos de una fuente de datos, como una API o una base de datos
     name: "John Doe",
     email: "johndoe@example.com",
     telephone: "1234567890",
